312c54690dc7be4d33da9fe9fd12f6b0fb020350
MisterNixon <resetlife@hotmail.ca>
2017-04-03 09:02:35 -0400

    exercices

---
+++ Exercices/exercice_B8/CompositeIcon.java
@@ -0,0 +1,55 @@
+package exercice_B8;
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+import javax.swing.Icon;
+
+@SuppressWarnings("serial")
+public class CompositeIcon extends ArrayList<Icon> implements Icon{
+
+	
+	@Override
+	public int getIconHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getIconWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void paintIcon(Component arg0, Graphics arg1, int arg2, int arg3) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void addIcon(Icon icon,int x, int y){
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
